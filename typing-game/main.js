b3ae5e9ea0b2dc1321629c08b58ece0ab12b1381
wangkodok <wangkodok@gmail.com>
2022-06-27 18:42:07 +0900

    chapter_18: 데이터상태처리

--- typing-game/main.js
+++ typing-game/main.js
@@ -4,8 +4,13 @@ const scoreDisplay = document.querySelector('#score');
 const timeeDisplay = document.querySelector('#time');
 const messageDisplay = document.querySelector('#message');
 
+const GAME_TIME = 5;
+
 let words = ['banana', 'key', 'car', 'javascript', 'scalper'];
 let score = 0;
+let time = 0;
+let timeInterval;
+let isPlaying = false;
 
 wordInput.addEventListener('input', (e) => {
     const typedText = e.target.value;
@@ -16,11 +21,33 @@ wordInput.addEventListener('input', (e) => {
     }
 })
 
+function gameover() {
+    isPlaying = false;
+    clearInterval(timeInterval);
+    timeInterval = null;
+    messageDisplay.innerText = 'GAME OVER!!!';
+    score = 0;
+}
+
+function countDown() {
+    time = time - 1;
+    timeeDisplay.innerText = time;
+    if (time === 0) {
+        gameover();
+    }
+}
+
 function setNewWord() {
+    time = GAME_TIME;
     wordInput.value = '';
     messageDisplay.innerText = 'Now Playing!!!';
     const randomIndex = Math.floor(Math.random() * words.length);
     currentWord.innerText = words[randomIndex];
+
+    if (!isPlaying) {
+        timeInterval = setInterval(countDown, 1000);
+        isPlaying = true;
+    }
 }
 
 function addScore() {
