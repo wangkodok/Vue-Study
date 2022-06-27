b2dc963c7cf80f94b3dfa3a584e26da0e611b74b
wangkodok <wangkodok@gmail.com>
2022-06-27 10:13:48 +0900

    chapter_14: 배열데이터 다루기

--- js-todo/main.js
+++ js-todo/main.js
@@ -3,7 +3,11 @@ const addButton = document.querySelector('.add-button');
 const list = document.querySelector('.list');
 const likeButtons = document.querySelectorAll('.like');
 
-addButton.addEventListener('click', function() {
+function addItem() {
+    if (inputText.value.trim() === '') {
+        return;
+    }
+
     // like
     const like = document.createElement('span');
     const likeIcon = document.createElement('i');
@@ -30,8 +34,35 @@ addButton.addEventListener('click', function() {
     manage.appendChild(clearIcon);
 
     const li = document.createElement('li');
+
+    // event
+    like.addEventListener('click', (e) => {
+        const target = e.target;
+        const text = target.innerText === 'favorite' ? 'favorite_border' : 'favorite';
+        target.innerText = text;
+    })
+    checkIcon.addEventListener('click', (e) => {
+        const target = e.target.parentNode.parentNode;
+        target.classList.add('done');
+    })
+    clearIcon.addEventListener('click', (e) => {
+        const target = e.target.parentNode.parentNode;
+        list.removeChild(target);
+    })
+
     li.appendChild(like);
     li.appendChild(item);
     li.appendChild(manage);
     list.appendChild(li);
+
+    inputText.value = '';
+    inputText.focus();
+}
+
+inputText.addEventListener('keypress', (e) => {
+    if (e.keyCode === 13) {
+        addItem();
+    }
 })
+
+addButton.addEventListener('click', addItem)
