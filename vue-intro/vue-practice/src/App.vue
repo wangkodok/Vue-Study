a6e78efb278ddd0fe0b72802d032f151f1c7a1c1
wangkodok <wangkodok@gmail.com>
2022-06-28 20:49:47 +0900

    chapter_26: 텍스트바인딩

--- vue-intro/vue-practice/src/App.vue
+++ vue-intro/vue-practice/src/App.vue
@@ -1,21 +1,34 @@
 <template>
-  <h1>Hello Vue!</h1>
+  <div>
+    <h1>Hello {{ username }} !! 만나서 반갑습니다.</h1>
+    <h1 v-text="`${username} 만나서 반갑습니다.`"></h1>
+    <p>{{ year }}</p>
+    <p>{{ user.name }}</p>
+    <p>직업은 {{ user.job }} 입니다.</p>
+    <p>{{ user.age }}</p>
+    <p v-html="button"></p>
+    <p v-html="button2"></p>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-  components: {}
-}
+  name: "App",
+  data() {
+    return {
+      username: "scalper!!!",
+      year: 2022,
+      user: {
+        name: "scalper",
+        job: "programmer",
+        age: 30,
+      },
+      button: "<button>click!</button>",
+      button2: `<button onclick="alert('ok')">click!</button>`,
+      // button2: `<button onclick='document.body.style="background-color: red; transform: rotate(180deg);"''>click!</button>`,
+    };
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
