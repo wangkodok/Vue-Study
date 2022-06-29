de4aa61fb644bb3d20b8b6596bfd22aca6da0774
wangkodok <wangkodok@gmail.com>
2022-06-29 19:10:31 +0900

    chapter_32: 메서드

--- vue-intro/vue-practice/src/App.vue
+++ vue-intro/vue-practice/src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <h1>Hello Vue!</h1>
-    <ul>
-      <template v-for="(city, index) in cities" :key="index">
-        <li v-if="city.province === '경상도'">
-          {{ city.name }}
-        </li>
-      </template>
-    </ul>
+    <h2>5년 후 {{ add(5) }}</h2>
+    <h2>10년 후 {{ add(10) }}</h2>
+    <h2>15년 후 {{ add(15) }}</h2>
+    <h2>20년 후 {{ add(20) }}</h2>
+    <h2>25년 후 {{ add(25) }}</h2>
+    <h2>동갑의 10명이 있다면 나의 총 합은 {{ multiply(age, 20) }}</h2>
+    <h2>{{ getTotalScore(100) }}</h2>
   </div>
 </template>
 
@@ -16,15 +16,27 @@ export default {
   name: "App",
   data() {
     return {
-      cities: [
-        { name: "서울", province: "경기도" },
-        { name: "대전", province: "충청도" },
-        { name: "대구", province: "경상도" },
-        { name: "부산", province: "경상도" },
-        { name: "인천", province: "경기도" },
-        { name: "광주", province: "전라도" },
-      ],
+      age: 30,
+      job: "front-end",
     };
+  },
+  methods: {
+    add(num) {
+      console.log(this);
+      console.log(num);
+      return this.age + num; // age 접근하려면 this 사용
+    },
+    multiply(num1, num2 = 10) {
+      return num1 * num2;
+    },
+    getTotalScore(num) {
+      console.log(this.job + "입니다.");
+      return this.multiply(num, num);
+    },
+    // add: (num) => {
+    //   console.log(this);
+    //   return this.age + num; // lexical scope 함수에 선언에 따라 스코프가 결정
+    // },
   },
 };
 </script>
