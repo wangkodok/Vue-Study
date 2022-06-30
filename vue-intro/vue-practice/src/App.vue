ee7f2c6b4abe84dc12689fc7e78796ea8f465023
wangkodok <wangkodok@gmail.com>
2022-06-30 15:12:03 +0900

    chapter_38: component

--- vue-intro/vue-practice/src/App.vue
+++ vue-intro/vue-practice/src/App.vue
@@ -1,60 +1,32 @@
 <template>
   <div>
-    <h1>Watchers</h1>
-    <h2>current money :: {{ money }}</h2>
-    <div>
-      <button @click="money = money + 100">earn money</button>
-      <button @click="money = money - 100">spend money</button>
-    </div>
-    <h2>{{ receit }}</h2>
-    <button @click="receit.food = receit.food + 500">buy food</button>
-    <hr />
-    <input type="text" v-model="userName" />
+    <!-- <GreetingUser username="scalper" /> -->
+    <GreetingUser :username="username" />
+    <GreetingUser username="dooli" />
+    <GreetingUser username="pororo" />
+    <GreetingUser username="monkey" />
+    <GreetingUser />
+    <button @click="changeName()">change</button>
   </div>
 </template>
 
 <script>
+import GreetingUser from "./components/GreetingUser.vue";
 export default {
   name: "App",
+  components: {
+    GreetingUser: GreetingUser,
+  },
   data() {
     return {
-      userName: "scalper",
-      money: 0,
-      receit: {
-        food: 3000,
-        fee: 2000,
-        fare: 10000,
-      },
+      username: "scalper",
     };
   },
-  computed: {},
-  watch: {
-    userName: {
-      handler(newValue) {
-        console.log(newValue, "newValue");
-      },
-      immediate: true,
-    },
-    // receit(newValue, oldValue) {
-    //   console.log("영수증에 값 변화가 있음", newValue, oldValue);
-    // },
-    receit: {
-      handler(newValue) {
-        console.log("영수증에 값 변화", newValue);
-      },
-      deep: true,
-    },
-    money(newValue, oldValue) {
-      if (newValue > 2000 && newValue > oldValue) {
-        console.log("mission complete", oldValue);
-      }
-      if (oldValue < 1500 && newValue < oldValue) {
-        console.log("save money!!");
-      }
+  methods: {
+    changeName() {
+      this.username = "DDoch";
     },
   },
-  directives: {},
-  methods: {},
 };
 </script>
 
---
+++ vue-intro/vue-practice/src/components/GreetingUser.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2>Hello {{ username }}!!</h2>
+</template>
+
+<script>
+export default {
+  // name: "Greeting User",
+  props: {
+    // username: String,
+    username: {
+      type: String,
+      default: "User!",
+    },
+  },
+};
+</script>
+
+<style></style>
