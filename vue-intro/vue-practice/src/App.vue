caea928507aeb85fdd9f945c179e5f61045b236a
wangkodok <wangkodok@gmail.com>
2022-06-30 21:21:36 +0900

    chapter_41: component4

--- vue-intro/vue-practice/src/App.vue
+++ vue-intro/vue-practice/src/App.vue
@@ -1,38 +1,39 @@
 <template>
   <div>
-    <!-- <ul>
-      <li v-for="prduct in products" :key="prduct.id">{{ prduct }}</li>
-    </ul> -->
-
-    <!-- <ProductList :products="products" /> -->
-    <GreetingUser id="greeting" />
+    <h2>Hello Component</h2>
+    <button @click="displayDetail = true">show detail</button>
+    <DetailView
+      v-if="displayDetail"
+      @closeDetail="close"
+      @sendData="showData"
+    />
   </div>
 </template>
 
 <script>
-// import ProductList from "./components/ProductList.vue";
-import GreetingUser from "./components/GreetingUser.vue";
+import DetailView from "./components/DetailView.vue";
 export default {
   name: "App",
   components: {
-    // ProductList,
-    GreetingUser,
+    DetailView,
   },
   data() {
     return {
-      // products: [
-      //   { id: 0, name: "tv", price: 500000, company: "lg" },
-      //   { id: 1, name: "전자렌지", price: 200000, company: "삼성" },
-      //   { id: 2, name: "가스오븐", price: 300000, company: "한화" },
-      //   { id: 3, name: "냉장고", price: 500000, company: "대우" },
-      //   { id: 4, name: "에어컨", price: 1000000, company: "해태" },
-      // ],
+      displayDetail: false,
     };
   },
   computed: {},
   watch: {},
   directives: {},
-  methods: {},
+  methods: {
+    close() {
+      this.displayDetail = false;
+      console.log("eas");
+    },
+    showData(data) {
+      console.log(data, "data");
+    },
+  },
 };
 </script>
 
