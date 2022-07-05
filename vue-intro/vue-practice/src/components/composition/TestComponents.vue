8dba9b802ba558189549d14855dc61fb52702059
wangkodok <wangkodok@gmail.com>
2022-07-05 11:25:33 +0900

    chapter_50: composition-method-model

--- vue-intro/vue-practice/src/components/composition/TestComponents.vue
+++ vue-intro/vue-practice/src/components/composition/TestComponents.vue
@@ -1,61 +1,39 @@
 <template>
-  <!-- <h2>{{ state.username }}</h2> -->
-  <!-- <h2>{{ state.age }}</h2> -->
+  <h2>{{ username }}</h2>
+  <button @click="changeName">ChangeUserName</button>
 
-  <!-- <h2>{{ username }}</h2> -->
-  <!-- <h2>{{ age }}</h2> -->
+  <h2>제품명: {{ name }}, 가격 {{ price }}</h2>
+  <button @click="changeProduct">제품 바꾸기</button>
 
-  <h2>{{ isHandsome }}</h2>
-  <h2>handsome? {{ isNotUgly }}</h2>
-
-  <h2>handsome? {{ isHandsome }}</h2>
-  <!-- <h3>{{ state.name }}</h3> -->
-  <h3>{{ name }}</h3>
+  <div>
+    <input type="text" v-model="username" />
+  </div>
 </template>
 
 <script>
-// import { reactive, toRefs } from "vue";
 import { ref, reactive, toRefs } from "vue";
 export default {
   name: "TestComponent",
   setup() {
-    let isHandsome = ref(false);
+    const username = ref("scalper");
+    function changeName() {
+      username.value = "Messi";
+    }
     const state = reactive({
-      name: "scalper",
-      age: 50,
-      job: "dev",
+      name: "TV",
+      price: 100,
     });
-    // let isNotUgly = isHandsome;
-    // isHandsome.value = true;
-    // console.log(isNotUgly);
+    function changeProduct() {
+      state.name = "세탁기";
+      state.price = 5000;
+    }
     return {
-      isHandsome,
+      username,
+      changeName,
+      changeProduct,
       ...toRefs(state),
-      // state,
-      // isNotUgly,
     };
-    // const state = reactive({
-    //   username: "Scalper",
-    //   age: 50,
-    // });
-    // return toRefs(state);
-
-    // return {
-    //   // state,
-    //   username: state.username,
-    //   age: state.age,
-    // };
-    // const username = "scalper";
-    // const username = "scalper";
-    // return {
-    //   username,
-    // };
   },
-  // data() {
-  //   return {
-  //     username: "Scalper",
-  //   };
-  // },
 };
 </script>
 
