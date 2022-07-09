3225536de6d06b6d0d1e4def56e02f4e98b84889
wangkodok <wangkodok@gmail.com>
2022-07-10 00:57:03 +0900

    chapter_52: composition-lifecycle

---
+++ vue-intro/vue-practice/src/components/composition/ChildComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <hr />
+  <h2>{{ fullname }}</h2>
+  <button @click="sendParent">child click</button>
+</template>
+
+<script>
+import { computed } from "vue";
+export default {
+  name: "ChildComponent",
+  props: ["firstname", "lastname"],
+  // computed: {
+  //   fullname() {
+  //     return `${this.firstname} ${this.lastname}`;
+  //   },
+  // },
+  setup(props, context) {
+    const fullname = computed(() => {
+      return `${props.firstname} ${props.lastname}`;
+    });
+    function sendParent() {
+      context.emit("sendParent");
+    }
+
+    // onMounted(() => {
+    //   inputRef.value.focus();
+    //   console.log("onMounted");
+    // });
+
+    return {
+      fullname,
+      sendParent,
+    };
+  },
+};
+</script>
+
+<style></style>
--- vue-intro/vue-practice/src/components/composition/TestComponents.vue
+++ vue-intro/vue-practice/src/components/composition/TestComponents.vue
@@ -1,84 +1,26 @@
 <template>
-  <h2>{{ username }}</h2>
-  <hr />
-  <div>
-    price <input type="number" v-model="price" /> amount
-    <input type="number" v-model="amount" />
-    <h3>Total Price :: {{ totalPrice }}</h3>
-  </div>
-  <hr />
-  <div>
-    first <input type="text" v-model="home.city" /> last<input
-      type="text"
-      v-model="last"
-    />
-    <h3>Full name :: {{ fullName }}</h3>
-  </div>
+  <h2>Hello LifeCycle</h2>
+  <!-- <input type="text" v-model="username" ref="inputRef" /> -->
+
+  <ChildComponent
+    firstname="Code"
+    lastname="Scalper"
+    @sendParent="sendParent"
+  />
 </template>
 
 <script>
-import { ref, computed, reactive, toRefs, watch } from "vue";
+import ChildComponent from "./ChildComponent.vue";
 export default {
   name: "TestComponent",
-  setup() {
-    const username = ref("scalper");
-    const price = ref(5000);
-    const amount = ref(1);
-    console.log(price);
-    const totalPrice = computed(() => {
-      return price.value + amount.value;
-    });
-    watch(price, (newValue, oldValue) => {
-      console.log(newValue, oldValue);
-    });
-    const state = reactive({
-      first: "Code",
-      last: "Scalper",
-      home: {
-        city: "Seoul",
-        type: "Apartment",
-      },
-    });
-    const fullName = computed(function () {
-      return `${state.first} ${state.last}`;
-    });
-    watch(
-      () => {
-        return { ...state.home };
-      },
-      (newValue, oldValue) => {
-        console.log(newValue, oldValue);
-      },
-      { deep: true }
-    );
-    watch(
-      () => {
-        return state.home;
-      },
-      (newValue, oldValue) => {
-        console.log(newValue, oldValue);
-      }
-    );
-    return {
-      username,
-      price,
-      amount,
-      totalPrice,
-      ...toRefs(state),
-      fullName,
-    };
+  components: {
+    ChildComponent,
   },
-  // data() {
-  //   return {
-  //     price: 5000,
-  //     amount: 1,
-  //   };
-  // },
-  // computed: {
-  //   totalPrice() {
-  //     return this.price * this.price;
-  //   },
-  // },
+  methods: {
+    sendParent() {
+      console.log("클릭하면 나와라!!!");
+    },
+  },
 };
 </script>
 
