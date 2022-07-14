74903fc67fdc526f1d621ea04cd1dd55ef8952f1
wangkodok <wangkodok@gmail.com>
2022-07-14 17:51:36 +0900

    chapter_54: deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
   module.exports = {
-      // publicPath: process.env.NODE_ENV === 'production' ? "/Vue-Study" : "/",
-      publicPath: process.env.NODE_ENV === 'production' ? "/Vue-Study/vue-intro/vue-practice" : "/",
+      publicPath: process.env.NODE_ENV === 'production' ? "/Vue-Study" : "/",
+      // publicPath: process.env.NODE_ENV === 'production' ? "/Vue-Study/vue-intro/vue-practice" : "/",
       outputDir: "docs",
   }
